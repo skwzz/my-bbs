f9dbff6e83e4237b998b54e8f5132b856d038c74
skwzz <kidol9108@gmail.com>
2024-07-21 17:44:53 +0900

    Initial commit

---
+++ src/main/java/com/skwzz/mybbs/MyBbsApplication.java
@@ -0,0 +1,13 @@
+package com.skwzz.mybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skwzz/mybbs/MyBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwzz.mybbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
